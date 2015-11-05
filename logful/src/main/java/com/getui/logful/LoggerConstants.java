f5fc87550dbba1934f9cc41576f099d8e3785a25
Keith Ellis <caokd@getui.com>
2015-11-05 09:53:00 +0800

    Change version.

--- logful/src/main/java/com/getui/logful/LoggerConstants.java
+++ logful/src/main/java/com/getui/logful/LoggerConstants.java
@@ -106,7 +106,7 @@ public class LoggerConstants {
     /**
      * 日志库当前版本.
      */
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "0.2.0";
 
     public static final int STATE_ALL = 0x00;
 
